f249cfb05a8c67d4b2638554282e7b8d8aa68816
Thomas Don <awerdx520@gmail.com>
2018-05-18 07:01:24 +0800

    添加登录页面成功

--- app/src/main/java/com/cuit/monitorapp/main/cuit/monitorapp/MainActivity.java
+++ app/src/main/java/com/cuit/monitorapp/main/cuit/monitorapp/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         add_btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SetGestureActivity.class);
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 //intent.putExtra("data", "mainActivity");
                 //intent.setClass(MainActivity.this, SetGestureActivity.class);
                 startActivity(intent);
