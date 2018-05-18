9900c2ef935c5957708cfc6e060340efad65f9e5
Thomas Don <awerdx520@gmail.com>
2018-05-19 02:52:40 +0800

    完善了登录功能，和手势密码功能。注册功能没有实现。修复了预览页面与实际显示不匹配的bug。 登录的验证功能没有实现，永远返回true，没有链接leancloud 服务器，所以不能在线查询。 TODO: 1. 实现注册功能 2. 链接leancould服务器 3. 登录验证功能

--- app/src/main/java/com/cuit/monitorapp/main/cuit/monitorapp/MainActivity.java
+++ app/src/main/java/com/cuit/monitorapp/main/cuit/monitorapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.cuit.monitorapp.main.cuit.monitorapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.cuit.monitorapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent();
+
+        if (isLogin()) {
+            intent.setClass(this, SetGestureActivity.class);
+            intent.putExtra("activityNum", 0);
+            startActivity(intent);
+        } else {
+            intent.setClass(this, LoginActivity.class);
+            startActivity(intent);
+        }
+
+    }
+
+    private boolean isLogin() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean login = sharedPreferences.getBoolean("login", false);
+
+        return login;
     }
 }
--- app/src/main/java/com/cuit/monitorapp/main/cuit/monitorapp/SignupActivity.java
+++ app/src/main/java/com/cuit/monitorapp/main/cuit/monitorapp/SignupActivity.java
@@ -3,6 +3,8 @@ package com.cuit.monitorapp.main.cuit.monitorapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.cuit.monitorapp.R;
+
 public class SignupActivity extends AppCompatActivity {
 
     @Override
